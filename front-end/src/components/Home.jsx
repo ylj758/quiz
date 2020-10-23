5f491af15d02ad317c3f060a80b364ae53417852
ylj <758766951@qq.com>
2020-10-23 11:23:56 +0800

    [ylj]:添加商品到购物车

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,33 +1,99 @@
 import React, {Component} from 'react'
 import './Home.css';
-import {Link} from 'react-router-dom';
+import {Table,Modal,Button} from 'antd';
+import Cart from '../images/cart.png'
 
 class Home extends Component {
   state = {
     products: {},
+    cartNum: 0,
+    visible:false,
+    cartProducts:[]
   };
 
-  async componentDidMount(){
+  async componentDidMount() {
     const result = await fetch("http://localhost:8080/products");
     const data = await result.json();
     this.setState({
-      products:data
+      products: data
     });
   }
+
+  async handleAddCart(e, product) {
+    this.setState({
+      cartNum: this.state.cartNum + 1,
+      cartProducts: this.state.cartProducts.push(product)
+    });
+
+  }
+
+  showCartModal() {
+    this.setState({
+      visible: true
+    })
+  }
+
+  async handleAddOrder(products){
+    const response = await fetch("http://localhost:8080/order/add",{
+      method:"POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(products)
+    })
+  }
+
+  columns = [
+    {
+      title: '商品',
+      dataIndex: 'name',
+      key: 'name'
+    },
+
+    ]
+
   render() {
+
     return (
       <div className="home">
         {
-          Object.values(this.state.products).map((product,index) =>{
+          Object.values(this.state.products).map((product, index) => {
             return <div key={index} className="pro-div">
-              <img src = {product.img}></img>
+              <img src={product.img}></img>
               <p className="pro-name">{product.name}</p>
               <p className="pro-price">单价：{product.price}{product.unit}</p>
               <input type="submit" value="+" className="link-order"
-                    key={index}></input>
+                     key={index} onClick={(e, product) => this.handleAddCart(e, product)}></input>
             </div>
-          } )
+          })
         }
+        <div className="cart">
+          <Button onClick={(e) => this.showCartModal(e)} className="cancel-btn">
+            <img className="cartImg" src={Cart}></img>
+            <label className="cartNum">{this.state.cartNum}</label>
+          </Button>
+          <Modal
+            title="购物车"
+            footer={null}
+            visible={this.state.visible}
+            width={400}
+            onCancel={() => this.setState({visible: false})}
+          >
+            <div className="modal-body">
+              <Table
+                columns={this.columns}
+                rowKey="id"
+                dataSource={this.state.cartProducts}
+                className="order-table"
+              />
+
+              <div className="btn-group">
+                <Button onClick={() => this.handleAddOrder(this.state.cartProducts)} >立即下单</Button>
+                <Button onClick={() => this.setState({cartProducts:[]})} >清空</Button>
+              </div>
+            </div>
+          </Modal>
+        </div>
       </div>
     );
   }
